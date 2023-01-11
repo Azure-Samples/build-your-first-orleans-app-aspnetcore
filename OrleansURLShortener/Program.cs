1eb9ea753d5a958a0ac9989a3cf4c89e748dde16
Alex Wolf <alexwolf@microsoft.com>
2023-01-11 12:10:47 -0500

    formatting

--- OrleansURLShortener/Program.cs
+++ OrleansURLShortener/Program.cs
@@ -18,32 +18,32 @@ app.MapGet("/", () => "Hello World!");
 
 app.MapGet("/shorten",
     async (IGrainFactory grains, HttpRequest request, string redirect) =>
-{
-    // Create a unique, short ID
-    var shortenedRouteSegment = Guid.NewGuid().GetHashCode().ToString("X");
-
-    // Create and persist a grain with the shortened ID and full URL
-    var shortenerGrain = grains.GetGrain<IUrlShortenerGrain>(shortenedRouteSegment);
-    await shortenerGrain.SetUrl(redirect);
-
-    // Return the shortened URL for later use
-    var resultBuilder = new UriBuilder($"{request.Scheme}://{request.Host.Value}")
     {
-    Path = $"/go/{shortenedRouteSegment}"
-    };
+        // Create a unique, short ID
+        var shortenedRouteSegment = Guid.NewGuid().GetHashCode().ToString("X");
 
-    return Results.Ok(resultBuilder.Uri);
-});
+        // Create and persist a grain with the shortened ID and full URL
+        var shortenerGrain = grains.GetGrain<IUrlShortenerGrain>(shortenedRouteSegment);
+        await shortenerGrain.SetUrl(redirect);
+
+        // Return the shortened URL for later use
+        var resultBuilder = new UriBuilder($"{request.Scheme}://{request.Host.Value}")
+        {
+        Path = $"/go/{shortenedRouteSegment}"
+        };
+
+        return Results.Ok(resultBuilder.Uri);
+    });
 
 app.MapGet("/go/{shortenedRouteSegment}",
     async (IGrainFactory grains, string shortenedRouteSegment) =>
-{
-    // Retrieve the grain using the shortened ID and redirect to the original URL        
-    var shortenerGrain = grains.GetGrain<IUrlShortenerGrain>(shortenedRouteSegment);
-    var url = await shortenerGrain.GetUrl();
+    {
+        // Retrieve the grain using the shortened ID and redirect to the original URL        
+        var shortenerGrain = grains.GetGrain<IUrlShortenerGrain>(shortenedRouteSegment);
+        var url = await shortenerGrain.GetUrl();
 
-    return Results.Redirect(url);
-});
+        return Results.Redirect(url);
+    });
 
 app.Run();
 // </Endpoints>
